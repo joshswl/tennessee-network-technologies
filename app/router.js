4f50ecdd879f11508486387371e2325d5e913c44
Josh Lee <joshswl@gmail.com>
2017-06-07 21:16:01 -0500

    replicated layout from other sites with TNT content

--- app/router.js
+++ app/router.js
@@ -7,6 +7,9 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('home', { path: '/' });
+  this.route('contact');
+  this.route('companies');
 });
 
 export default Router;
