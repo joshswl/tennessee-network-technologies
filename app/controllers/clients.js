d864814360d75bd27a464d093668ad6c9d69e43a
Caleb Lee <caleb.gwl@gmail.com>
2017-07-12 23:47:58 -0500

    Load financial and hospitality logos, set up clients controller

--- app/controllers/clients.js
+++ app/controllers/clients.js
@@ -2,35 +2,37 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   retail: [
-    {
-      src: '/img/logos/abercrombie.png',
-    },
-    {
-      src: '/img/logos/hollister.png',
-    },
-    {
-      src: '/img/logos/old-navy.png',
-    },
-    {
-      src: '/img/logos/cvs.png',
-    },
-    {
-      src: '/img/logos/walgreens.png',
-    },
-    {
-      src: '/img/logos/dollar-general.png',
-    },
-    {
-      src: '/img/logos/dicks-sg.png',
-    },
-    {
-      src: '/img/logos/bab.png',
-    },
-    {
-      src: '/img/logos/ae.png',
-    },
-    {
-      src: '/img/logos/bebe.png',
-    },
-  ]
+    {src: '/img/logos/retail/abercrombie.png'},
+    {src: '/img/logos/retail/hollister.png'},
+    {src: '/img/logos/retail/old-navy.png'},
+    {src: '/img/logos/retail/cvs.png'},
+    {src: '/img/logos/retail/walgreens.png'},
+    {src: '/img/logos/retail/dollar-general.png'},
+    {src: '/img/logos/retail/dicks-sg.png'},
+    {src: '/img/logos/retail/bab.png'},
+    {src: '/img/logos/retail/ae.png'},
+    {src: '/img/logos/retail/bebe.png'},
+  ],
+  financial: [
+    {src: '/img/logos/financial/boa.png'},
+    {src: '/img/logos/financial/morgan-chase.png'},
+  ],
+  hospitality: [
+    {src: '/img/logos/hospitality/holiday-inn.png'},
+    {src: '/img/logos/hospitality/red-roof.png'},
+    {src: '/img/logos/hospitality/budget.jpg'},
+    {src: '/img/logos/hospitality/avis.png'},
+    {src: '/img/logos/hospitality/hertz.png'},
+  ],
+  foodService: [
+    {src: '/img/logos/food-service/olive-garden.png'},
+    {src: '/img/logos/food-service/mcdonalds.png'},
+    {src: '/img/logos/food-service/arbys.png'},
+    {src: '/img/logos/food-service/burgerking.png'},
+  ],
+  healthCare: [
+    {src: '/img/logos/health/hospitals.png'},
+    {src: '/img/logos/health/dialysis.png'},
+    {src: '/img/logos/health/long-term.png'},
+  ],
 });
