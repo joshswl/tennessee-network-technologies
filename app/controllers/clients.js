76b1015c6e6d2a7f4ed5d0bb9b82c772eeba47e4
Caleb Lee <caleb.gwl@gmail.com>
2017-07-12 19:49:54 -0500

    Retail logos and grid on clients page

---
+++ app/controllers/clients.js
@@ -0,0 +1,36 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  retail: [
+    {
+      src: '/img/logos/abercrombie.png',
+    },
+    {
+      src: '/img/logos/hollister.png',
+    },
+    {
+      src: '/img/logos/old-navy.png',
+    },
+    {
+      src: '/img/logos/cvs.png',
+    },
+    {
+      src: '/img/logos/walgreens.png',
+    },
+    {
+      src: '/img/logos/dollar-general.png',
+    },
+    {
+      src: '/img/logos/dicks-sg.png',
+    },
+    {
+      src: '/img/logos/bab.png',
+    },
+    {
+      src: '/img/logos/ae.png',
+    },
+    {
+      src: '/img/logos/bebe.png',
+    },
+  ]
+});
